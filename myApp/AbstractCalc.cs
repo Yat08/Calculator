bac06bdf1f8affdac644f0959ce5a407a2f914e6
Yat08 <95243863+Yat08@users.noreply.github.com>
2021-11-29 21:33:05 +0200

    Create AbstractCalc.cs

---
+++ myApp/AbstractCalc.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace myApp
+{
+    class AbstractCalc
+    {
+        public string GetMake() {}
+        /*public double num1 { get; set; }
+        public double num2 { get; set; }*/
+
+        public AbstractCalc() { }
+
+        public double add() { 
+
+        }
+
+        public double sub() { 
+
+        }
+
+        public double mul() { 
+            
+        }
+
+        public double div() { 
+   
+        }
+    }
+
+}
